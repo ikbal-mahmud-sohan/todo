0efe1fa70e24bd946040db23d2ef5fd7f98a7d8d
Sohan Mahmud <sohan@codemen.net>
2021-11-12 16:01:38 +0600

    modified text

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func main() {
 	h := handler.New()
 	http.HandleFunc("/", h.Home)
 	http.HandleFunc("/todos/create",h.Todos )
-	fmt.Println("Server starting")
-	if err := http.ListenAndServe("127.0.0.1:5731", nil); err != nil {
+	fmt.Println("Server starting with port 7000 ")
+	if err := http.ListenAndServe("127.0.0.1:7000", nil); err != nil {
 		log.Fatal(err)
 	}
 }
